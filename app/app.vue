63243d956fae0c9380e9fa23fe681dd39077d96a
Roman <garbromannik@gmail.com>
2024-09-09 06:40:00 +0300

    indicator

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <NuxtLoadingIndicator
+      color="green"
+      :height="4"
+      style="opacity: 100 !important"
+    />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
